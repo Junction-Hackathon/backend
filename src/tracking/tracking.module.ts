675ae315e13d5d7c5eee65f25437d057c4abc021
zaki <z.goumri@esi-sba.dz>
2025-07-18 09:19:38 +0100

    feature(implement tracking): implement real time tracking for the sacrifices

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TrackingService } from './tracking.service';
 import { TrackingController } from './tracking.controller';
+import { TrackingGateway } from './tracking.gateway';
 
 @Module({
   controllers: [TrackingController],
-  providers: [TrackingService],
+  providers: [TrackingService, TrackingGateway],
 })
 export class TrackingModule {}
